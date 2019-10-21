0fd89232d629a81fc3b10a18179aa809a1a11097
Daniel Sendula <natdan@users.noreply.github.com>
2019-10-21 16:52:53 +0100

    Started work towards multiused game

--- gcc/core/src/org/ah/gcc/virtualrover/VisibleObject.java
+++ gcc/core/src/org/ah/gcc/virtualrover/VisibleObject.java
@@ -1,10 +1,11 @@
 package org.ah.gcc.virtualrover;
 
-import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+
 import org.ah.themvsus.engine.common.game.GameObject;
 
 public interface VisibleObject {
 
-    void render(Batch batch);
+    void render(ModelBatch batch);
     void update(GameObject gameObject);
 }
